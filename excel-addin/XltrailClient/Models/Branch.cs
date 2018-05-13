ff68a5f77fbb8a3110dde9548075302b05a8b6d7
Bjoern Stiel <bjoern.stiel@zoomeranalytics.com>
2018-05-13 15:42:50 +0100

    Added Excel addin

---
+++ excel-addin/XltrailClient/Models/Branch.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Xltrail.Client.Models
+{
+    public class Branch
+    {
+        public static string StagingBranchPrefix = "staging__";
+
+        public Workbook Workbook { get; private set; }
+        public string Id { get; private set; }
+        public string Name { get; private set; }
+        public string Path { get; private set; }
+        public string FileName { get; private set; }
+
+        public Branch(Workbook workbook, string branch)
+        {
+            Id = "id-" + Convert.ToBase64String(Guid.NewGuid().ToByteArray()).Replace("+", "").Replace("/", "_").Replace(" ", "").Replace("=", "");
+            Workbook = workbook;
+            Name = branch;
+            FileName = System.IO.Path.GetFileNameWithoutExtension(Workbook.Path) + "_" + DisplayName + (IsStagingBranch? "_local" : "") + System.IO.Path.GetExtension(Workbook.Path);
+            Path = System.IO.Path.Combine(Workbook.Path, FileName).Replace("\\", "/");
+        }
+
+        public bool IsStagingBranch
+        {
+            get
+            {
+                return Name.StartsWith(StagingBranchPrefix) && Regex.Matches(Name, StagingBranchPrefix).Count % 2 != 0;
+            }
+        }
+
+        public string BranchId
+        {
+            get
+            {
+                var history = GetHistory().ToList();
+                var firstCommit = history.Last();
+
+                //TODO: this should be a function of the filename at first commit
+                return history.Last().Commit.Sha;
+            }
+        }
+
+        public void Commit(string path, string userName, string email)
+        {
+            var fileRepositoryPath = System.IO.Path.Combine(Workbook.Repository.Path.Replace("/", "\\"), Workbook.Path.Replace("/", "\\"));
+
+            //checkout branch
+            LibGit2Sharp.Commands.Checkout(Workbook.Repository.GitRepository, Name);
+
+            //copy file
+            File.Copy(path, fileRepositoryPath, true);
+
+            //stage
+            LibGit2Sharp.Commands.Stage(Workbook.Repository.GitRepository, fileRepositoryPath);
+
+            //prepare commit details
+            var author = new LibGit2Sharp.Signature(userName, email, DateTime.Now);
+            var commitOptions = new LibGit2Sharp.CommitOptions();
+            var commitMessage = "Updated " + Workbook.Path;
+            var committer = author;
+
+            //commit
+            Workbook.Repository.GitRepository.Commit(commitMessage, author, committer, commitOptions);
+        }
+
+        public void Discard()
+        {
+            if (!IsStagingBranch)
+                throw new Exception("Can only discard staging branch");
+
+            //checkout branch
+            LibGit2Sharp.Commands.Checkout(Workbook.Repository.GitRepository, OtherBranchName);
+
+            //delete Git staging branch
+            Workbook.Repository.GitRepository.Branches.Remove(Name);
+
+            //delete branch from workbook
+            Workbook.Branches.Remove(this);
+        }
+
+        private string GetStagingBranchName()
+        {
+            return StagingBranchPrefix + BranchId + "__" + Name;
+        }
+
+        public Branch Checkout()
+        {
+            if (IsStagingBranch)
+                throw new Exception("Cannot check out staging branch");
+
+            var stagingBranchName = GetStagingBranchName();
+            var libGit2Branch = Workbook.Repository.GitRepository.Branches[Name];
+
+            //create staging branch if not exists
+            if (Workbook.Repository.GitRepository.Branches[stagingBranchName] == null)
+            {
+                Workbook.Repository.GitRepository.Branches.Update(
+                    Workbook.Repository.GitRepository.Branches.Add(stagingBranchName, libGit2Branch.Tip),
+                    b => b.TrackedBranch = libGit2Branch.CanonicalName);
+
+                Workbook.Branches.Add(new Branch(Workbook, stagingBranchName));
+            }
+
+            //checkout staging branch
+            LibGit2Sharp.Commands.Checkout(Workbook.Repository.GitRepository, stagingBranchName);
+            return Workbook.Branches.Where(branch => branch.Name == stagingBranchName).FirstOrDefault();
+        }
+
+        public string DisplayName
+        {
+            get
+            {
+                if (!IsStagingBranch)
+                    return Name;
+
+                return Name.Substring(StagingBranchPrefix.Length + BranchId.Length + 2);
+            }
+        }
+
+        public string LastModified
+        {
+            get
+            {
+                return GetHeadCommit().Author.When.ToLocalTime().DateTime.ToString("MMMM dd, yyyy, HH:MM:ss");
+            }
+        }
+
+        public string Description
+        {
+            get
+            {
+                var head = GetHeadCommit();
+                if (!IsStagingBranch)
+                {
+                    return DisplayName + " [last modified by " + head.Author.Name + ", " + LastModified + "]" + (HasConflict ? "*" : "");
+                }
+                return DisplayName + " [" + head.Author.Name + ", " + head.Author.When.ToLocalTime().DateTime.ToString("MMMM dd, yyyy, HH:MM:ss") + "]" + (HasConflict ? "*" : "");
+            }
+        }
+
+
+        private string OtherBranchName
+        {
+            get
+            {
+                if (!IsStagingBranch)
+                    return GetStagingBranchName();
+
+                return Name.Substring(StagingBranchPrefix.Length + BranchId.Length + 2);
+            }
+        }
+
+    public Branch OtherBranch
+        {
+            get
+            {
+                //find corresponding `other` branch
+                var otherBranch = Workbook.Branches.Where(b => b.Name == OtherBranchName).FirstOrDefault();
+
+                return otherBranch;
+            }
+        }
+
+        public bool HasConflict
+        {
+            get
+            {
+                //find corresponding `other` branch
+                var otherBranch = OtherBranch;
+
+                //circuit breaker
+                if (otherBranch == null)
+                    return false;
+
+                return otherBranch.Head != Head;
+            }
+        }
+
+        private IEnumerable<LibGit2Sharp.LogEntry> GetHistory()
+        {
+            return Workbook
+             .Repository
+             .GitRepository
+             .Commits
+             .QueryBy(Workbook.Path, new LibGit2Sharp.CommitFilter { IncludeReachableFrom = Name });
+        }
+
+        public LibGit2Sharp.Commit GetHeadCommit()
+        {
+            return Workbook
+             .Repository
+             .GitRepository
+             .Commits
+             .QueryBy(Workbook.Path, new LibGit2Sharp.CommitFilter { IncludeReachableFrom = Name })
+             .First().Commit;
+        }
+
+
+        public string Head
+        {
+            get
+            {
+                return GetHeadCommit().Sha;
+            }
+            
+        }
+    }
+}
---
+++ excel-addin/XltrailClient/Models/Repository.cs
@@ -0,0 +1,182 @@
+﻿using System.Collections.Generic;
+using LibGit2Sharp;
+using System.Linq;
+using System;
+
+namespace Xltrail.Client.Models
+{
+    public class Repository
+    {
+        public LibGit2Sharp.Repository GitRepository { get; private set; }
+        public string Id { get; private set; }
+        public string Path { get; private set; }
+        public string Name { get; private set; }
+        public IList<Workbook> Workbooks { get; private set; }
+        public Dictionary<string, string> Folders { get; private set; }
+
+        public Repository(string path)
+        {
+            Id = CreateId();
+            Path = path;
+            Name = System.IO.Path.GetFileName(path);
+            GitRepository = new LibGit2Sharp.Repository(path);
+            InitialiseWorkbooks();
+        }
+
+        private string CreateId()
+        {
+            return "id-" + Convert.ToBase64String(Guid.NewGuid().ToByteArray()).Replace("+", "").Replace("/", "_").Replace(" ", "").Replace("=", "");
+        }
+
+        public string Url
+        {
+            get
+            {
+                return GitRepository.Network.Remotes["origin"].PushUrl;
+            }
+        }
+
+        public static bool IsValid(string path)
+        {
+            return LibGit2Sharp.Repository.IsValid(path);
+        }
+
+        public Workbook GetWorkbookById(string id)
+        {
+            return Workbooks.Where(workbook => workbook.Id == id).FirstOrDefault();
+        }
+
+        public Branch GetWorkbookBranchById(string id)
+        {
+            foreach(var workbook in Workbooks)
+            {
+                foreach(var branch in workbook.Branches)
+                {
+                    if (branch.Id == id)
+                        return branch;
+                }
+            }
+            return null;
+        }
+
+        public static List<string> TraverseFolders(string path)
+        {
+            var separator = '/';
+            var parts = path.Split(separator);
+            var folders = new List<string>();
+
+            foreach (var part in parts)
+            {
+                if(folders.Count == 0)
+                {
+                    folders.Add(part);
+                }
+                else
+                {
+                    folders.Add(folders.Last() + separator + part);
+                }
+            }
+
+            if (folders[0] != "")
+                folders.Insert(0, "");
+
+            return folders;
+        }
+
+        private void InitialiseWorkbooks()
+        {
+            Folders = new Dictionary<string, string>();
+            Workbooks = new List<Workbook>();
+            foreach (var branch in GitRepository.Branches.Where(branch => !branch.IsRemote))
+            {
+                Commands.Checkout(GitRepository, branch);
+                foreach (var file in GitRepository.Index)
+                {
+                    if (IsWorkbook(file.Path))
+                    {
+                        var workbook = Workbooks.Where(x => x.Path == file.Path).FirstOrDefault();
+                        if (workbook == null)
+                        {
+                            workbook = new Workbook(this, file.Path);
+                            Workbooks.Add(workbook);
+                            foreach(var folder in TraverseFolders(workbook.Folder))
+                            {
+                                if (!Folders.ContainsKey(folder))
+                                {
+                                    Folders[folder] = CreateId();
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private bool IsWorkbook(string path)
+        {
+            return (
+                path.EndsWith(".xls") ||
+                path.EndsWith(".xlsx") ||
+                path.EndsWith(".xlsb") ||
+                path.EndsWith(".xlsm") ||
+                path.EndsWith(".xla") ||
+                path.EndsWith(".xlam"));
+        }
+
+        public Branch GetWorkbookBranchFromPath(string path)
+        {
+            foreach(var workbook in Workbooks)
+            {
+                var branch = workbook.Branches.Where(b => b.Path == path).FirstOrDefault();
+                if (branch != null)
+                    return branch;
+            }
+            return null;
+        } 
+
+
+        public IList<Workbook> GetWorkbooks(string path)
+        {
+            var files = new List<Workbook>();
+            if (path == null)
+                return files;
+
+            foreach (var workbook in Workbooks)
+            {
+                if (workbook.Path.StartsWith(path))
+                {
+                    if (path == System.IO.Path.GetDirectoryName(workbook.Path).Replace("\\", "/"))
+                    {
+                        files.Add(workbook);
+                    }
+                }
+            }
+            return files;
+        }
+
+        public IList<string> GetFolders(string path)
+        {
+            var folders = new List<string>();
+            if (path == null)
+                return folders;
+
+            foreach (var workbook in Workbooks)
+            {
+                if (workbook.Path.StartsWith(path))
+                {
+                    var p = System.IO.Path.GetDirectoryName(workbook.Path).Replace("\\", "/");
+                    if (p != path)
+                    {
+                        var subPath = workbook.Path.Substring(path.Length);
+                        if (subPath.StartsWith("/"))
+                            subPath = subPath.Substring(1);
+                        var f = subPath.Split('/').First();
+                        if (f != null)
+                            folders.Add(f);
+                    }
+                }
+            }
+            return folders.Distinct().OrderBy(x => x).ToList();
+        }
+    }
+}
